68a06ce77409761594c8afd73f2772181aa4bd06
AlexanderThomason <abt36@duke.edu>
2021-09-13 21:42:38 -0400

    added y_intercept_calculator() function to calculate the y-intercept given a point and a slope

--- line_calculator.py
+++ line_calculator.py
@@ -7,9 +7,17 @@ def line_calculator(point1_tuple,point2_tuple,x_point_value):
     y_point_value = slope* x_point_value + y_intercept
     return y_point_value
 
+
 def slope_calculator(point1_tuple,point2_tuple):
     slope = (point2_tuple[1]-point1_tuple[1]) / (point2_tuple[0]-point1_tuple[0])
     return slope
+
+
+def y_intercept_calculator(point_tuple,slope):
+    y_intercept = point_tuple[1] - slope * point_tuple[0]
+    return y_intercept
+
+
 
 if __name__ == "__main__":
     y_point = line_calculator((0,2),(2,6), 2)

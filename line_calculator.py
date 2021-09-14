407931c4f3e20194023b1f2f5ea4951bbac89d9c
AlexanderThomason <abt36@duke.edu>
2021-09-13 22:51:50 -0400

    added comments to the line_calculator code

--- line_calculator.py
+++ line_calculator.py
@@ -1,13 +1,14 @@
 # line_calculator.py
 # Author: Alex Thomason
 
+## Non-modular function 
 def line_calculator(point1_tuple,point2_tuple,x_point_value):
     slope = (point2_tuple[1]-point1_tuple[1]) / (point2_tuple[0]-point1_tuple[0]) 
     y_intercept = point2_tuple[1] - slope * point2_tuple[0]
     y_point_value = slope* x_point_value + y_intercept
     return y_point_value
     
-
+# Start of modular code
 def slope_calculator(point1_tuple,point2_tuple):
     slope = (point2_tuple[1]-point1_tuple[1]) / (point2_tuple[0]-point1_tuple[0])
     return slope

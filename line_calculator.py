188e1db8456e897d5ea35f892878bc1cab359850
AlexanderThomason <abt36@duke.edu>
2021-09-13 21:33:23 -0400

    added slope_calculator() function to calculate the slope between two points

--- line_calculator.py
+++ line_calculator.py
@@ -7,6 +7,9 @@ def line_calculator(point1_tuple,point2_tuple,x_point_value):
     y_point_value = slope* x_point_value + y_intercept
     return y_point_value
 
+def slope_calculator(point1_tuple,point2_tuple):
+    slope = (point2_tuple[1]-point1_tuple[1]) / (point2_tuple[0]-point1_tuple[0])
+    return slope
 
 if __name__ == "__main__":
     y_point = line_calculator((0,2),(2,6), 2)

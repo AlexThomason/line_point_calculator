a12963106f52044fc89758257fc8a1d914a67450
AlexanderThomason <abt36@duke.edu>
2021-09-13 22:14:53 -0400

    added lines to run throught the modular code to calculate the y-point

--- line_calculator.py
+++ line_calculator.py
@@ -6,7 +6,7 @@ def line_calculator(point1_tuple,point2_tuple,x_point_value):
     y_intercept = point2_tuple[1] - slope * point2_tuple[0]
     y_point_value = slope* x_point_value + y_intercept
     return y_point_value
-
+    
 
 def slope_calculator(point1_tuple,point2_tuple):
     slope = (point2_tuple[1]-point1_tuple[1]) / (point2_tuple[0]-point1_tuple[0])
@@ -24,6 +24,10 @@ def y_point_calculator(slope,y_intercept,x_point_value):
 
 
 if __name__ == "__main__":
-    y_point = line_calculator((0,2),(2,6), 2)
+    y_point1 = line_calculator((0,2),(2,6), 2)
     print(y_point)
 
+    slope = slope_calculator((0,2),(2,6))
+    y_intercept = y_intercept_calculator((0,2),slope)
+    y_point2 = y_point_calculator(slope,y_intercept,2)
+    print(y_point2)
